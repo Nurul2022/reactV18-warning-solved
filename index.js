1604ae3732f25f954b8a8111570d4517066c3adb
Nurul Islam <nurulbdpersonal@gmail.com>
2022-04-06 23:42:46 +0600

    React V18 Warning sloved code

---
+++ index.js
@@ -0,0 +1,17 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
+
+reportWebVitals();
+
+// delete all data index.js in your project then...
+// above 14 line data copy and paste to index.js in your project 
